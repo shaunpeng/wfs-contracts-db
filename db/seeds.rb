9adcc41397265c23cea56d18f672bd12ebfe092a
Shaun P <shaun.peng@gmail.com>
2017-01-20 15:29:47 +0000

    afterfooter

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+puts "Importing suppliers..."
+CSV.foreach(Rails.root.join("suppliers.csv"), headers: true) do |row|
+  Supplier.create! do |supplier|
+    supplier.name = row[0]
+  end
+end
