5b6c83a01178abb1176f78799a33ee4720009f53
Shaun P <shaun.peng@gmail.com>
2017-01-25 07:20:17 +0000

    aftershortlist

--- app/assets/javascripts/lines.js
+++ app/assets/javascripts/lines.js
@@ -1,27 +1,58 @@
 $(document).on('turbolinks:load', function() {
-  var product;
-  product = $('#prod-name').html();
-  $('#prod-category').change(function() {
-    var product_category, options, escaped_category;
+    var product_shorthtml = $('#prod-name-shortlist').html();
+    var product_longhtml = $('#prod-name-longlist').html();
+
+    $('#prod-category').change(function() { // trigger
+        var product_category, escaped_category, options_short, options_long;
+        product_category = $('#prod-category :selected').text();
+        if (product_category !='') {
+            escaped_category = product_category.replace(/([ #;&,.+*~\':"!^$[\]()=>|\/@])/g, '\\$1');
+            options_short = $(product_shorthtml).filter("optgroup[label=" + escaped_category + "]").prepend("<option selected></option>").html();
+            options_long = $(product_longhtml).filter("optgroup[label=" + escaped_category + "]").prepend("<option selected></option>").html();
+            $('#prod-name-shortlist').html(options_short);
+            $('#prod-name-longlist').html(options_long);
+        } else {
+            confirm("Please select a product category!");
+        }
+    });
+    
+    var product_category, escaped_category;
     product_category = $('#prod-category :selected').text();
     if (product_category !='') {
         escaped_category = product_category.replace(/([ #;&,.+*~\':"!^$[\]()=>|\/@])/g, '\\$1');
-        options = $(product).filter("optgroup[label=" + escaped_category + "]").prepend("<option selected></option>").html();
-        return $('#prod-name').html(options);
-    } else {
-      confirm("Please select a product category!");
+        var options_short = $(product_shorthtml).filter("optgroup[label=" + escaped_category + "]").prepend("<option selected></option>").html();
+        var options_long = $(product_longhtml).filter("optgroup[label=" + escaped_category + "]").prepend("<option selected></option>").html();
+        $('#prod-name-shortlist').html(options_short);
+        $('#prod-name-longlist').html(options_long);
     }
-  });
+    
+    // if (document.getElementById('shortlist-checkbox').checked) {
+        document.getElementById('prod-name-longlist').setAttribute("disabled", "disabled");
+        document.getElementById('prod-name-shortlist').removeAttribute("disabled");
+        $('#prod-name-shortlist').show();
+        $('#prod-name-longlist').hide();
+    // } else {
+    //   //confirm("unchecked");
+    //     document.getElementById('prod-name-shortlist').setAttribute("disabled", "disabled");
+    //     document.getElementById('prod-name-longlist').removeAttribute("disabled");
+    //     $('#prod-name-longlist').show();
+    //     $('#prod-name-shortlist').hide();
+    // }
+    
 });
 
-$(document).on('turbolinks:load', function() {
-  var product;
-  product = $('#prod-name').html();
-  var product_category, options, escaped_category;
-  product_category = $('#prod-category :selected').text();
-  if (product_category !='') {
-      escaped_category = product_category.replace(/([ #;&,.+*~\':"!^$[\]()=>|\/@])/g, '\\$1');
-      options = $(product).filter("optgroup[label=" + escaped_category + "]").prepend("<option selected></option>").html();
-      return $('#prod-name').html(options);
-  }
-});
+function handleClick(cb) {
+    if (cb.checked) {
+      //confirm("checked");
+        document.getElementById('prod-name-longlist').setAttribute("disabled", "disabled");
+        document.getElementById('prod-name-shortlist').removeAttribute("disabled");
+        $('#prod-name-shortlist').show();
+        $('#prod-name-longlist').hide();
+    } else {
+      //confirm("unchecked");
+        document.getElementById('prod-name-shortlist').setAttribute("disabled", "disabled");
+        document.getElementById('prod-name-longlist').removeAttribute("disabled");
+        $('#prod-name-longlist').show();
+        $('#prod-name-shortlist').hide();
+    }
+}
--- app/models/prod_category.rb
+++ app/models/prod_category.rb
@@ -1,4 +1,9 @@
 class ProdCategory < ApplicationRecord
     has_many :products, -> { order("products.name") }
     belongs_to :line, optional: true
+    
+    def usualproducts
+        products.where short_list: 'Y'
+    end
+    
 end
